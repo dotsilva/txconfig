55fe1923bca3f4c518376beec43367d0069196aa
dotsilva <devbrunodasilva@gmail.com>
2025-11-02 17:18:04 -0300

    trying to fix the bug

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,13 +1,9 @@
 #!/bin/env bash
 
 #update dotfiles, you can replace the ones in txconfig/dotfiles/ , just remind it must be one file for each filetype, only one .ttf, only one .properties...
-cd ~/txconfig/
-git pull
 cp -r ~/txconfig/dotfiles/*.ttf ~/.termux/font.ttf
 cp -r ~/txconfig/dotfiles/*.properties ~/.termux/colors.properties
 cp -r ~/txconfig/dotfiles/tmux.conf ~/.tmux.conf
 cp -r ~/txconfig/dotfiles/profile ~/.profile
 termux-reload-settings
 source ~/.profile
-echo teste
-cd ~/
