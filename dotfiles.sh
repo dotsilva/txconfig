4fbd822a45a0a83805bc47e4809a564c7dd70975
dotsilva <devbrunodasilva@gmail.com>
2025-11-02 17:11:45 -0300

    trying to fix a error in termux while txdeping

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 
 #update dotfiles, you can replace the ones in txconfig/dotfiles/ , just remind it must be one file for each filetype, only one .ttf, only one .properties...
-cd ~/txconfig/ && git pull && cd
+cd ~/txconfig/ && git pull
+cd ~/
 cp -r ~/txconfig/dotfiles/*.ttf ~/.termux/font.ttf
 cp -r ~/txconfig/dotfiles/*.properties ~/.termux/colors.properties
 cp -r ~/txconfig/dotfiles/tmux.conf ~/.tmux.conf
